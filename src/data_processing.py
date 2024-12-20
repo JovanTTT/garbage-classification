2bf4216c858bcf6fac4628a03e941ae184a6a073
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-12-20 17:47:52 +0100

    first model training

--- src/data_processing.py
+++ src/data_processing.py
@@ -8,71 +8,77 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 
-data_train_path = 'Data/train'
-data_test_path = 'Data/test'
-data_val_path = 'Data/val'
+# data_train_path = 'Data/train'
+# data_test_path = 'Data/test'
+# data_val_path = 'Data/val'
 
-img_width = 180
-img_height = 180
+def process_data(data_train_path, data_test_path, data_val_path):
+    img_width = 180
+    img_height = 180
 
-data_train = tf.keras.utils.image_dataset_from_directory( #ucitava slike u train, 1672
+    data_train = tf.keras.utils.image_dataset_from_directory( #ucitava slike u train, 1672
     data_train_path,
     shuffle = True,
     image_size = (img_width, img_height),
     batch_size = 32,
     validation_split = False
-)
+    )
 
-data_val = tf.keras.utils.image_dataset_from_directory(#358
+    data_val = tf.keras.utils.image_dataset_from_directory(#358
     data_val_path,
     image_size = (img_height, img_width),
     batch_size = 32,
     shuffle = False,
     validation_split = False
-)
+    )
 
-data_test = tf.keras.utils.image_dataset_from_directory(#360
+    data_test = tf.keras.utils.image_dataset_from_directory(#360
     data_test_path,
     image_size = (img_width, img_height),
     shuffle = False,
     batch_size = 32,
     validation_split = False
-)
+    )
 
-data_cat = data_train.class_names #imena kategorija u data_cat
+    data_cat = data_train.class_names
 
-
-# data_iterator = data_train.as_numpy_iterator()
-# batch = data_iterator.next()
-# fig, ax = plt.subplots(ncols=4, figsize=(20,20))
-# for idx, img in enumerate(batch[0][:4]):
-#      ax[idx].imshow(img.astype(int))
-#      ax[idx].title.set_text(batch[1][idx])
-#      plt.show()
+    return data_train, data_val, data_test, data_cat
 
 
 
-print(f"Broj klasa: {len(data_cat)}")
-print(f"Imena klasa: {data_cat}")
+def process_data_scaled(data_train_path, data_val_path, data_test_path):
+    """Processes and scales the data."""
+    img_width = 180
+    img_height = 180
 
-# Ukupan broj slika u trening skupu
-train_images_count = len(data_train.file_paths)
-print(f"Trening podaci: {train_images_count}")
+    data_train = tf.keras.utils.image_dataset_from_directory(
+        data_train_path,
+        shuffle=True,
+        image_size=(img_width, img_height),
+        batch_size=32
+    )
 
-# Ukupan broj slika u validacionom skupu
-val_images_count = len(data_val.file_paths)
-print(f"Validacija podaci: {val_images_count}")
+    data_val = tf.keras.utils.image_dataset_from_directory(
+        data_val_path,
+        shuffle=False,
+        image_size=(img_width, img_height),
+        batch_size=32
+    )
 
-# Ukupan broj slika u test skupu
-test_images_count = len(data_test.file_paths)
-print(f"Test podaci: {test_images_count}")
+    data_test = tf.keras.utils.image_dataset_from_directory(
+        data_test_path,
+        shuffle=False,
+        image_size=(img_width, img_height),
+        batch_size=32
+    )
 
-#Ovo se koristi kao novi dataset
-data_test_scale = data_test.map(lambda x,y: (x/255, y)) #vrednosti piksela se skaliraju od 0 do 1, bolje funkcionisu kad su podaci normalizovani
-data_test_scale.as_numpy_iterator().next()
+    data_cat = data_train.class_names
 
-data_train_scale = data_train.map(lambda x,y: (x/255, y))
-data_train_scale.as_numpy_iterator().next()
+    # Apply normalization (scaling)
+    data_train_scaled = data_train.map(lambda x, y: (x / 255.0, y))
+    data_val_scaled = data_val.map(lambda x, y: (x / 255.0, y))
+    data_test_scaled = data_test.map(lambda x, y: (x / 255.0, y))
 
-data_val_scale = data_val.map(lambda x,y: (x/255, y))
-data_val_scale.as_numpy_iterator().next()
+    return data_train_scaled, data_val_scaled, data_test_scaled, data_cat
+
+
