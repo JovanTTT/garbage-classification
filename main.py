423b979c05683b3af298ad8a0c70a7b9e24c603b
Milica <milicasalajic13@gmail.com>
2025-09-15 17:02:07 +0200

    changed function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.data_processing import process_data_first
+from src.data_processing import process_data
 from src.data_processing import show_sample_images_with_predictions
 from train_model.train_model2.train_model2 import build_efficientnet, train_model
 from train_model.train_model1 import build_cnn, train_cnn_model
@@ -9,7 +9,7 @@ from train_model.train_model_video import classify_detected_frames, create_annot
 
 
 # Preprocess data
-train_data, val_data, test_data, class_names = process_data_first("Data/train", "Data/val", "Data/test")
+train_data, val_data, test_data, class_names = process_data("Data/train", "Data/val", "Data/test")
 
 #Define class weights (optional, based on dataset analysis)
 #Klase s manjim brojem uzoraka dobijaju vece tezine da bi bilo jednako
@@ -35,27 +35,27 @@ class_weights = {0: 1.0, 1: 2.0, 2: 1.5, 3: 1.8, 4: 1.2}
 
 
 
-# # CNN model
-# model_type = 'cnn'
-# model = build_cnn()
-#
-# # Train the model
-# trained_model, history = train_cnn_model(model, train_data, val_data, epochs=15, class_weights=class_weights)
-#
-# # Save the trained model
-# trained_model.save(f"models/{model_type}_nonscaled.h5")
-# print(f"Model saved to models/{model_type}.h5")
+# CNN model
+model_type = 'cnn'
+model = build_cnn()
+
+# Train the model
+trained_model, history = train_cnn_model(model, train_data, val_data, epochs=5, class_weights=class_weights)
+
+# Save the trained model
+trained_model.save(f"models/{model_type}_nonscaled.h5")
+print(f"Model saved to models/{model_type}.h5")
 
 
 # CCT model
-model_type = 'cct'
-model = build_cct()
+#model_type = 'cct'
+#model = build_cct()
 
 # Train the model
-trained_model, history = train_cct(model, train_data, val_data, epochs=35)
+#trained_model, history = train_cct(model, train_data, val_data, epochs=35)
 
 # Save the trained model
-trained_model.save(f"models/{model_type}_1.h5")
+#trained_model.save(f"models/{model_type}_1.h5")
 print(f"Model saved to models/{model_type}_1.h5")
 
 
@@ -70,19 +70,19 @@ print(f"Model saved to models/{model_type}_1.h5")
 # print(f"Test accuracy: {test_accuracy}")
 #
 #
-# show_sample_images_with_predictions(trained_model, train_data, class_names)
+show_sample_images_with_predictions(trained_model, train_data, class_names)
 #
-# # Video classification
-# selected_frames = [5.0, 12.0, 20.0, 29.0, 37.0]  # The time points you want to use
-#
-# video_path = 'Data/video/classification_video.mp4'
-#
-# # classification with images
-# classify_detected_frames(trained_model, video_path, selected_frames, class_names)
-#
-# # clasification with new video
-# output_path = 'Data/video/annotated_classification_video.mp4'
-# create_annotated_video(video_path, trained_model, class_names, selected_frames, output_path)
+# Video classification
+selected_frames = [5.0, 12.0, 20.0, 29.0, 37.0]  # The time points you want to use
+
+video_path = 'Data/video/classification_video.mp4'
+
+# classification with images
+classify_detected_frames(trained_model, video_path, selected_frames, class_names)
+
+# clasification with new video
+output_path = 'Data/video/annotated_classification_video.mp4'
+create_annotated_video(video_path, trained_model, class_names, selected_frames, output_path)
 
 
 
